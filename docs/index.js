19adbcc515d1f59adb59ed1098f0e60d22c1d0fa
BaconErie <baconerie0001@gmail.com>
2022-06-20 19:53:23 -0400

    Mobile topbar complete

--- docs/index.js
+++ docs/index.js
@@ -1,8 +1,18 @@
 let moreButton = document.getElementById('more-button');
 let mobileMenu = document.getElementById('mobile-menu');
+let moreImage = document.getElementById('more-button-image');
+let lessImage = document.getElementById('less-button-image');
 
 function moreButtonClicked() {
-    mobileMenu.style.display = 'flex';
+    if(mobileMenu.style.display != 'flex'){
+        mobileMenu.style.display = 'flex';
+        moreImage.style.display = 'none';
+        lessImage.style.display = 'initial';
+    }else{
+        mobileMenu.style.display = 'none';
+        moreImage.style.display = 'initial';
+        lessImage.style.display = 'none';
+    }
 }
 
 moreButton.addEventListener('click', moreButtonClicked)
