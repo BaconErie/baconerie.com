41b8d5eb5ee4cdfd2b496502d845ffbfeacd9c0e
BaconErie <baconerie0001@gmail.com>
2024-03-14 18:19:52 -0400

    Revamp website

---
+++ app/layout.tsx
@@ -0,0 +1,33 @@
+import { ThemeProvider } from "@/components/theme-provider"
+import { Open_Sans as FontSans } from "next/font/google";
+import "./globals.css";
+
+const fontSans = FontSans({
+    subsets: ["latin"],
+    variable: "--font-sans",
+})
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <head>
+        <title>BaconErie</title>
+        <link rel="icon" type="image/x-icon" href="/rounded-pfp.png" />
+      </head>
+      <body className={fontSans.className}>
+        <ThemeProvider
+            attribute="class"
+            defaultTheme="system"
+            enableSystem
+            disableTransitionOnChange
+          >
+        {children}
+        </ThemeProvider>
+      </body>
+    </html>
+  );
+}
---
+++ components/ui/separator.tsx
@@ -0,0 +1,6 @@
+"use client"
+
+export default function Separator()
+{
+  return <div className="rounded-lg bg-rainbow animate-gradient-move w-full h-2"></div>
+}
